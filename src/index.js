0a2e4a1f20ec41714cfabb27de8f7ce47a33c9f1
tindoann <tindoann@gamil.com>
2020-07-11 23:45:48 -0400

    working app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  action,
+  createStore,
+  StoreProvider,
+  useStore,
+  useActions
+} from "easy-peasy";
 
+import "./styles.css";
+import { Likes } from "./Likes";
+import { LikesAndComments } from "./LikesAndComments";
+
+const store = createStore({
+  data: {
+    likes: 0,
+    numComments: 0,
+    incrementLikes: action(state => {
+      state.likes++;
+    }),
+    incrementNumComments: action(state => {
+      state.numComments++;
+    })
+  }
+});
+
+function App() {
+  const data = useStore(state => state.data);
+  const { incrementLikes, incrementNumComments } = useActions(
+    actions => actions.data
+  );
+
+  return (
+    <div>
+      <button onClick={incrementLikes}>increment likes</button>
+      <button onClick={incrementNumComments}>increment numComments</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <LikesAndComments />
+      <br />
+      <Likes />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreProvider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StoreProvider>,
+  rootElement
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// https://www.youtube.com/watch?v=5gUHfe-ETuo
